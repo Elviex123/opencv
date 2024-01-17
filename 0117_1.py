d244dff6fd23deedf4500f67330caea94767cdc5
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-01-17 14:34:14 +0800

    1

--- 0117_1.py
+++ 0117_1.py
@@ -1,6 +1,17 @@
 import cv2
+"""photo
 img = cv2.imread('meme.jpg')
-img = cv2.resize(img,(300,300))
+img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
 cv2.imshow('img',img)
 
 cv2.waitKey(0)
+"""
+cap = cv2.VideoCapture('video1.mp4')
+
+while True:
+    ret, frame =cap.read()
+    if ret:
+        cv2.imshow('video',frame)
+    else:
+        break
+    cv2.waitKey(1)
